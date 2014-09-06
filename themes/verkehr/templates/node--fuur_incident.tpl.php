f0e800e555be7c35fbff0623f91c7e572d33687b
Mikkel Hoegh <mikkel@hoegh.org>
2014-09-06 23:03:08 +0200

    Moving, preparing for install profile conversion.

---
+++ themes/verkehr/templates/node--fuur_incident.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @file
+ * Theme implementation for incident nodes.
+ */
+dpm($variables);
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+røghul
+
+  <header>
+    <?php print render($title_prefix); ?>
+    <?php if (!$page): ?>
+      <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a></h2>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+
+    <?php print render($content['fuur_incident_location']); ?>,
+    <?php print render($content['fuur_incident_datetime']); ?>
+  </header>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <?php print render($content['links']); ?>
+  <?php print render($content['comments']); ?>
+
+</article>
